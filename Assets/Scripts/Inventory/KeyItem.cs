b4502d59374475ab7474888607aaa086cf237d62
OliverKoll <oliverkolltveit@gmail.com>
2022-06-02 20:09:57 +0200

    Added key items

--- Assets/Scripts/Inventory/KeyItem.cs
+++ Assets/Scripts/Inventory/KeyItem.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyItem : MonoBehaviour
+[CreateAssetMenu(menuName = "Items/Create new key")]
+public class KeyItem : ItemBase
 {
     //Should be used as quest items to unlock new places or finish quests
+
+    public override bool UsableInBattle => false;
+    public override bool UsableOutsideBattle => false;
 }
