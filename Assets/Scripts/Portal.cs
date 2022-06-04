a800080c39eec2377cc6f6146d7eeda4ccdf15f0
OliverKoll <oliverkolltveit@gmail.com>
2022-06-05 01:21:51 +0200

    Added level requirement to portals

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,14 +4,28 @@ using UnityEngine.SceneManagement;
 public class Portal : Collidable
 {
     public string[] sceneNames;
+
+    [SerializeField] int levelRequired;
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.tag == "Player")
         {
-            // Teleport the player
-            GameManager.instance.SaveState();
-            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
-            SceneManager.LoadScene(sceneName);
+            int playerLevel = coll.gameObject.GetComponent<UnitList>().GetPlayerUnit().Level;
+            if(checkRequirements(playerLevel))
+            {
+                // Teleport the player
+                GameManager.instance.SaveState();
+                string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+                SceneManager.LoadScene(sceneName);
+            }
         }
+    }
+
+    bool checkRequirements(int characterLevel)
+    {
+        if(levelRequired > characterLevel)
+            return false;
+
+        return true;
     }
 }
